8c9d37deeaceb31531e330ea10c9a58f5d50a1bd
mksnkv <makosenkov@gmail.com>
2019-05-28 22:54:35 +0300

    lab4 report

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import signal
 
 
-def get_plot(x, y, x_label, y_label, title, show, save, close):
+def get_plot(x, y, x_label, y_label, title, show, save, close, pic_name):
     plt.xlabel(x_label)
     plt.ylabel(y_label)
     plt.title(title)
     plt.plot(x, y)
     plt.grid(True)
-    if show:
-        plt.show()
     if save:
-        plt.savefig(title + '.png')
+        plt.savefig(pic_name + '.png')
     if close:
         plt.close()
 
@@ -21,22 +20,12 @@ def get_fft_signal(num, sampling, sig):
     fft_sig = np.fft.fft(sig) / num * 2
     # частота
     freq_fft = np.fft.fftfreq(num, 1 / sampling)
-    lim = 1000
+    lim = 1500
     return fft_sig, freq_fft, lim
 
 
-def plot_am(t, message_signal, modulated):
-    lim = 150
-    get_plot(t[:lim], modulated[:lim],
-             x_label='Time', y_label='Amplitude', title='AM',
-             show=False, save=False, close=False)
-    get_plot(t[:lim], message_signal[:lim],
-             x_label='Time', y_label='Amplitude', title='AM',
-             show=True, save=False, close=True)
-
-
 def signal_synthesis():
-    fs = 2000
+    fs = 1500
     number = 4096
     t = np.arange(0, number / fs, 1 / fs)
     message_freq = 20
@@ -44,24 +33,57 @@ def signal_synthesis():
     # исходный сигнал
     message_signal = amplitude * np.sin(2 * np.pi * message_freq * t)
     carrier_freq = 500
-    # M < 1
-    carrier_signal = 0.5 * np.cos(2 * np.pi * carrier_freq * t)
-    modulated = message_signal * carrier_signal
-    plot_am(t, message_signal, modulated)
-    # M > 1
-    carrier_signal = 2 * np.cos(2 * np.pi * carrier_freq * t)
-    modulated = message_signal * carrier_signal
-    plot_am(t, message_signal, modulated)
-    # M = 1
-    carrier_signal = np.cos(2 * np.pi * carrier_freq * t)
-    modulated = message_signal * carrier_signal
-    plot_am(t, message_signal, modulated)
+    carrier_signal = np.sin(2 * np.pi * carrier_freq * t)
 
+    # ==========АМПЛИТУДНАЯ===========
+    modulated = (1 + 0.5 * amplitude * message_signal) * carrier_signal
+    lim = 200
+    get_plot(t[:lim], modulated[:lim],
+             x_label='Time', y_label='Amplitude', title='AM, M < 1',
+             show=True, save=True, close=True, pic_name='am1_sig')
+
+    modulated = (1 + 1.5 * amplitude * message_signal) * carrier_signal
+    get_plot(t[:lim], modulated[:lim],
+             x_label='Time', y_label='Amplitude', title='AM, M > 1',
+             show=True, save=True, close=True, pic_name='am2_sig')
+
+    modulated = (1 + amplitude * message_signal) * carrier_signal
+    get_plot(t[:lim], modulated[:lim],
+             x_label='Time', y_label='Amplitude', title='AM, M = 1',
+             show=True, save=True, close=True, pic_name='am3_sig')
     sig_fft, fft_freq, lim = get_fft_signal(number, fs, modulated)
-    # график спектра сигнала
+    # график спектра сигнала с модуляцией
     get_plot(x=fft_freq[:lim], y=sig_fft[:lim], x_label='Frequency',
-             y_label='Amplitude', title='Spectrum plot',
-             show=True, save=False, close=True)
+             y_label='Amplitude', title='AM spectrum',
+             show=True, save=True, close=True, pic_name='am_spectrum')
+
+    # ==========С ПОДАВЛЕНИЕМ НЕСУЩЕЙ===========
+    suppressed_modulated = message_signal * carrier_signal
+    lim = 190
+    get_plot(t[:lim], suppressed_modulated[:lim],
+             x_label='Time', y_label='Amplitude', title='Supressed modulation',
+             show=True, save=True, close=True, pic_name='sup_sig')
+    sig_fft, fft_freq, lim = get_fft_signal(number, fs, suppressed_modulated)
+    # график спектра сигнала с модуляцией
+    get_plot(x=fft_freq[:lim], y=sig_fft[:lim], x_label='Frequency',
+             y_label='Amplitude', title='Supressed spectrum',
+             show=True, save=True, close=True, pic_name='sup_spectrum')
+
+    # ==========ОДНОПОЛОСНАЯ===========
+    single_mod = signal.hilbert(message_signal) * np.cos(2 * np.pi * carrier_freq * t) - \
+                 signal.hilbert(message_signal) * carrier_signal
+    lim = 190
+    get_plot(t[:lim], single_mod[:lim],
+             x_label='Time', y_label='Amplitude', title='Singleband modulation',
+             show=False, save=False, close=False, pic_name='single_sig')
+    get_plot(t[:lim], message_signal[:lim],
+             x_label='Time', y_label='Amplitude', title='Singleband modulation',
+             show=True, save=True, close=True, pic_name='single_sig')
+    sig_fft, fft_freq, lim = get_fft_signal(number, fs, single_mod)
+    # график спектра сигнала с модуляцией
+    get_plot(x=fft_freq[:lim], y=sig_fft[:lim], x_label='Frequency',
+             y_label='Amplitude', title='Single band spectrum',
+             show=True, save=True, close=True, pic_name='single_spectrum')
 
 
 if __name__ == '__main__':
